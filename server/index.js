9cd7c0d8ce7b46be976d9d6e4005ffc5a5233c4b
George <gulubovgeorge@gmail.com>
2026-01-07 10:37:45 +0200

    supabase database conection

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// TEST ROUTE: backend
 app.get('/api', (req, res) => {
     res.json({status: 'Backend is running'});
 });
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-});
+app.listen(PORT, () => 
+    console.log(`Server running on http://localhost:${PORT}`));
+
+exportDefault = (testConection());
