ac03533945d2dba3c6675134b6fd90119cfddf59
George <gulubovgeorge@gmail.com>
2026-01-20 17:13:53 +0200

    renovation and rearrangement of the entire project

--- server/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error(" ERROR MESSAGE:", err.message);
-  console.error(" ERROR STACK:", err.stack);
-
-  res.status(500).json({
-    error: err.message || "Internal Server Error",
-  });
-};
--- server/middleware/logger.js
+++
@@ -1,4 +0,0 @@
-export const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`); // showing request in console
-  next(); // going to next route
-};
