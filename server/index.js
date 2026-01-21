32ff85601e5365559b74114094275529551fc90c
George <gulubovgeorge@gmail.com>
2026-01-21 17:07:17 +0200

    mvp authService.js file

--- server/index.js
+++ server/index.js
@@ -1,26 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 
-import walletRoutes from './routes/walletRoutes.js';
-import transactionsRoutes from './routes/transactionsRoutes.js';
-import { authenticate } from './middleware/auth.js';
-import { logger } from './middleware/logger.js';
-import { errorHandler } from './middleware/errorHandler.js';
-
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true}));// frontend dev port credentials: true
 app.use(express.json());
-
-// Loging request
-app.use(logger)
-
-// Routes
-app.use('/api/wallet', authenticate, walletRoutes);
-app.use('/api/transactions', authenticate, transactionsRoutes);
-
-// Error Handler
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ server/services/authService.js
@@ -0,0 +1,52 @@
+import { supabase } from "../config/supabase.js";
+
+export async function checkUserByEmail(email) {
+
+    const { data, error } = await supabase
+        .from("users")       // select from the "users" table
+        .select("*")         // select all columns
+        .eq("email", email)  // filter where "email" equals the given email
+        .limit(1);           // limit the result to 1 row
+
+    if (error) throw new Error("Database error"); // throw an error if the query fails
+
+    return data[0] || null; // return the first row if it exists, otherwise return null
+};
+
+
+export async function signUp(fullName, email, password) {
+    const existingUser = await checkUserByEmail(email); // Check if a user with the given email already exists
+
+    if (existingUser) throw new Error("User already exists"); // Throw an error if user already exists
+
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password before storing it in the database
+
+    const { data, error } = await supabase.from("users").insert({ // Insert the new user into the "users" table
+        full_name: fullName,           // store the full name
+        email,                         // store the email
+        password: hashedPassword       // store the hashed password
+    });
+
+    if (error) throw new Error("Failed to create user"); // Throw an error if inserting the user failed
+
+    return data[0]; // Return the first row of the inserted user
+};
+
+
+export async function signIn(email, password) {
+    
+    const existingUser = await checkUserByEmail(email); // Find user by email
+
+    if (!existingUser) {
+        throw new Error("User not found");
+    }
+
+     const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+    if (!isPasswordCorrect) {
+        throw new Error("Incorrect password"); 
+    }
+
+    return existingUser;
+
+}
